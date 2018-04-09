e40099b1c6c7cf4b13b1c38eaf332c7563d2e603
Brandon <brandonantoniocook@gmail.com>
2018-04-08 21:40:29 -0400

    Auto termination of user SSH sessions

--- YARN/console/Brain_Integration/TermServer1.0.py
+++ YARN/console/Brain_Integration/TermServer1.0.py
@@ -7,6 +7,7 @@
 
 import os
 import datetime
+from netmiko import ConnectHandler
 
 #------------------------------------------------------
 #CREATE MERGE FILES PER POD BASIS
@@ -23,24 +24,24 @@ def mediaryConfigAdditions(podNumber, Password):
     f.close()
     NAPALM_MERGE('1')
     os.remove('pod1.cfg')
-  
+
   if podNumber == 'pod2':
     open('pod2.cfg', 'w').close()
     f=open("pod2.cfg", "a")
-    for x in range(8,16):      
+    for x in range(8,16):
           f.write('!\nline 1/' + str(x) + '\n  exec-timeout 0 0\n  password ' + Password)
           f.write('\n  login authentication PODS\n  rotary ' + str(x+2) + '\n  no exec\n  transport input ssh\n')
-    f.close() 
+    f.close()
     NAPALM_MERGE('2')
     os.remove('pod2.cfg')
-  
+
   if podNumber == 'pod3':
     open('pod3.cfg', 'w').close()
     f=open("pod3.cfg", "a")
     for x in range(16,24):
           f.write('!\nline 1/' + str(x) + '\n  exec-timeout 0 0\n  password ' + Password)
           f.write('\n  login authentication PODS\n  rotary ' + str(x+4) + '\n  no exec\n  transport input ssh\n')
-    f.close()    
+    f.close()
     NAPALM_MERGE('3')
     os.remove('pod3.cfg')
 
@@ -119,3 +120,36 @@ def NAPALM_MERGE(num):
   except e:
     print (e)
     file.write(str(now) + e + "Failpoint: Closing the Connection...")
+
+  #------------------------------------------------------
+  #Kick off prior users
+  #------------------------------------------------------
+
+  #Set device connection params
+  termServ = {
+      'device_type': 'cisco_ios',
+      'ip': '172.20.5.4',
+      'username': 'console01',
+      'password': '7554306100$#@!',
+      'port': 2222,
+  }
+  #Connect to device
+  net_connect = ConnectHandler(**termServ)
+  net_connect.find_prompt()
+
+  #Close prior connections
+  if num == 'pod1':
+    for x in range(0,8):
+          output = net_connect.send_command_timing("clear line 1/" + str(x))
+          if output == '[confirm]':
+              output += net_connect.send_command_timing("y")
+  if num == 'pod2':
+    for x in range(8,16):
+          output = net_connect.send_command_timing("clear line 1/" + str(x))
+          if output == '[confirm]':
+              output += net_connect.send_command_timing("y")
+  if num == 'pod3':
+    for x in range(16,24):
+          output = net_connect.send_command_timing("clear line 1/" + str(x))
+          if output == '[confirm]':
+              output += net_connect.send_command_timing("y")
